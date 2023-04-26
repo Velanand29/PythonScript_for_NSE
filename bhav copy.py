6df9ae9360c7aa9d17e0419c2562e6b0c8ddb23a
Velanand29 <91517092+Velanand29@users.noreply.github.com>
2023-04-26 15:35:53 +0530

    Working With hard coded date for to downlaod bhavcopy but cant get f&O list

--- bhav copy.py
+++ bhav copy.py
@@ -1,8 +1,5 @@
-# import requests
-import zipfile
-
 from jugaad_data.nse import bhavcopy_save
-from datetime import datetime  as datetime # Python
+from datetime import datetime  as date 
 
-bhavcopy_save(datetime(2023,4,25),"C:\Users\anand\Downloads")
+bhavcopy_save(date(2023,4,25),r"C:\Users\anand\OneDrive\Documents\Bhav Copy")
 
