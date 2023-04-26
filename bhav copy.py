57003785986d43383bfb5c04ed71b0aebdc5e980
Velanand29 <91517092+Velanand29@users.noreply.github.com>
2023-04-26 16:58:22 +0530

    Now can get the F&O copy

--- bhav copy.py
+++ bhav copy.py
@@ -1,5 +1,19 @@
-from jugaad_data.nse import bhavcopy_save
+import datetime
+from jugaad_data.nse import bhavcopy_save 
+from jugaad_data.nse import bhavcopy_fo_save 
+
 from datetime import datetime  as date 
 
-bhavcopy_save(date(2023,4,25),r"C:\Users\anand\OneDrive\Documents\Bhav Copy")
+# Get the current date and time
+current_datetime = datetime.datetime.now()
+
+# Get yesterday's date
+yesterday_date = current_datetime - datetime.timedelta(days=1)
+
+#GET 
+#bhavcopy_save(date(2023,4,24),r"C:\Users\anand\OneDrive\Documents\Bhav Copy")
+bhavcopy_fo_save(date(2023,4,25),r"C:\Users\anand\OneDrive\Documents\Bhav Copy")
+
+
+
 
