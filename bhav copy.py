dd5fe0fdaae75c59d956c8fc8588f6af849af3aa
Velanand29 <91517092+Velanand29@users.noreply.github.com>
2023-05-25 21:19:41 +0530

    Added selection option

--- bhav copy.py
+++ bhav copy.py
@@ -5,17 +5,21 @@ import tkinter as tk
 from tkinter import filedialog
 from datetime import date
 from datetime import time 
-from jugaad_data.nse import bhavcopy_save, bhavcopy_fo_save , bhavcopy_fo_raw ,bhavcopy_index_raw,bhavcopy_index_save,bhavcopy_raw,full_bhavcopy_raw,full_bhavcopy_save
+from jugaad_data.nse import bhavcopy_save, bhavcopy_fo_save, bhavcopy_fo_raw, bhavcopy_index_raw, bhavcopy_index_save, bhavcopy_raw, full_bhavcopy_raw, full_bhavcopy_save
 
-# Download bhavcopy for futures and options
-#Format (YYYY,M,DD)
-#Do Not use 2023-08-02  use 2023-5-9
-#Download Using current Date 
-x = datetime.datetime.now()
-bhavcopy_fo_save(date(int(x.year),int(x.month),int(x.day)), r"C:\Users\anand\OneDrive\Documents\BHAV COPY")
+choice = int(input("Enter 1 to download using the current date or 2 to enter a specific date: "))
 
-#Use to get date from the User
-y = int(input("Enter the Year :"))
-m = int(input("Enter the Month :"))
-d = int(input("Enter the Day :"))
-bhavcopy_fo_save(date(y, m, d), r"C:\Users\anand\OneDrive\Documents\BHAV COPY")
+if choice == 1:
+    # Download bhavcopy for futures and options
+    # Format (YYYY,M,DD)
+    # Do Not use 2023-08-02, use 2023-5-9
+    # Download Using current Date 
+    x = datetime.datetime.now()
+    bhavcopy_fo_save(date(int(x.year), int(x.month), int(x.day)), r"C:\Users\anand\OneDrive\Documents\BHAV COPY")
+elif choice == 2:
+    y = int(input("Enter the Year: "))
+    m = int(input("Enter the Month: "))
+    d = int(input("Enter the Day: "))
+    bhavcopy_fo_save(date(y, m, d), r"C:\Users\anand\OneDrive\Documents\BHAV COPY")
+else:
+    print("Invalid choice! Please enter 1 or 2.")
