7c385348dbefef94b15fe52f713dc3ee33208d54
Velanand29 <91517092+Velanand29@users.noreply.github.com>
2023-05-25 21:31:54 +0530

    Added selection option + To dowload like n number of days but it will work only with in a week

--- bhav copy.py
+++ bhav copy.py
@@ -4,16 +4,21 @@ import urllib.request
 import tkinter as tk
 from tkinter import filedialog
 from datetime import date
-from datetime import time 
+from datetime import timedelta
 from jugaad_data.nse import bhavcopy_save, bhavcopy_fo_save, bhavcopy_fo_raw, bhavcopy_index_raw, bhavcopy_index_save, bhavcopy_raw, full_bhavcopy_raw, full_bhavcopy_save
 
-choice = int(input("Enter 1 to download using the current date or 2 to enter a specific date: "))
+def download_bhavcopy(num_days):
+    end_date = datetime.datetime.now().date()
+    start_date = end_date - timedelta(days=num_days - 1)
+    
+    current_date = start_date
+    while current_date <= end_date:
+        bhavcopy_fo_save(current_date, r"C:\Users\anand\OneDrive\Documents\BHAV COPY")
+        current_date += timedelta(days=1)
+
+choice = int(input("Enter 1 to download using the current date,***2 to enter a specific date, or 3 to download data for a specific number of days: "))
 
 if choice == 1:
-    # Download bhavcopy for futures and options
-    # Format (YYYY,M,DD)
-    # Do Not use 2023-08-02, use 2023-5-9
-    # Download Using current Date 
     x = datetime.datetime.now()
     bhavcopy_fo_save(date(int(x.year), int(x.month), int(x.day)), r"C:\Users\anand\OneDrive\Documents\BHAV COPY")
 elif choice == 2:
@@ -21,5 +26,8 @@ elif choice == 2:
     m = int(input("Enter the Month: "))
     d = int(input("Enter the Day: "))
     bhavcopy_fo_save(date(y, m, d), r"C:\Users\anand\OneDrive\Documents\BHAV COPY")
+elif choice == 3:
+    num_days = int(input("Enter the number of days to download the bhavcopy: "))
+    download_bhavcopy(num_days)
 else:
-    print("Invalid choice! Please enter 1 or 2.")
+    print("Invalid choice! Please enter 1, 2, or 3.")
